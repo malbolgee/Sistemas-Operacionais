57654e2e104f32187e70be67bb4d25dfcf54b7ea
victor hugo <victor.gomes@icomp.ufam.edu.br>
2020-10-24 00:57:01 -0400

    Versão final
    - Muitas modificações feitas
    - Verificação de overflow
    - Nova Documentação
    - Refatoração

--- Alocador/aloca.cpp
+++ Alocador/aloca.cpp
@@ -4,10 +4,11 @@
 #include <vector>
 #include <algorithm>
 #include <utility>
+#include <cassert>
 #include "aloca.h"
 
 #ifdef __DEBUG__
-    #define DEBUG
+#define DEBUG
 #endif
 
 meualoc::meualoc(int tamanhoMemoria, int policitaMem)
@@ -16,17 +17,11 @@ meualoc::meualoc(int tamanhoMemoria, int policitaMem)
     if (policitaMem > NEXTFIT)
         policitaMem = NEXTFIT;
 
-    this->ptr_nextfit = 0;
+    this->ptr_nextfit = 0UL;
     this->politica = policitaMem;
     this->memoria = (char *)malloc(sizeof(char) * (tamanhoMemoria));
     memset(this->memoria, 0, sizeof(char) * (tamanhoMemoria));
     this->livres.push_back(std::make_pair(this->memoria, tamanhoMemoria));
-
-    #ifdef DEBUG
-        puts("Blocos livres depois de alocação:");
-        for (auto t : this->livres)
-            printf("Endereço: %p tamanho: %hu\n", t.first, t.second);
-    #endif
 }
 
 char *meualoc::aloca(unsigned short tamanho)
@@ -38,16 +33,15 @@ char *meualoc::aloca(unsigned short tamanho)
 char *meualoc::bestfit(unsigned short tamanho)
 {
 
-    #ifdef DEBUG
-        printf("%s\n", __func__);
-    #endif
+    if ((ushort)(tamanho + (ushort)0x4) < tamanho)
+        return NULL;
 
     tamanho += 4;
-    int index = 0;
+    size_t index = 0UL;
     bool found = false;
     ushort menorTamaho = 0xFFFF;
 
-    for (int i = 0; i < this->livres.size(); ++i)
+    for (size_t i = 0UL; i < this->livres.size(); ++i)
     {
 
         if (tamanho <= this->livres[i].second)
@@ -68,17 +62,15 @@ char *meualoc::bestfit(unsigned short tamanho)
 char *meualoc::firstfit(unsigned short tamanho)
 {
 
-    #ifdef DEBUG
-        printf("%s\n", __func__);
-    #endif
+    if ((ushort)(tamanho + (ushort)0x4) < tamanho)
+        return NULL;
 
     tamanho += 4;
-    int index = 0;
+    size_t index = 0UL;
     bool found = false;
-
     size_t size = this->livres.size();
 
-    for (int i = 0; i < size; ++i)
+    for (size_t i = 0UL; i < size; ++i)
         if (tamanho <= this->livres[i].second)
             found = true, index = i, i = size;
 
@@ -92,26 +84,17 @@ char *meualoc::firstfit(unsigned short tamanho)
 char *meualoc::nextfit(unsigned short tamanho)
 {
 
-    #ifdef DEBUG
-        printf("%s\n", __func__);
-    #endif
+    // Overflow?
+    if ((ushort)(tamanho + (ushort)0x4) < tamanho)
+        return NULL;
 
     tamanho += 4;
     bool found = false;
-
     size_t size = this->livres.size();
 
-    for (int i = this->ptr_nextfit; i < size; ++i)
+    for (size_t i = this->ptr_nextfit; i < size; ++i)
         if (tamanho <= this->livres[i].second)
             found = true, this->ptr_nextfit = i, i = size;
-
-    if (!found)
-    {
-        this->ptr_nextfit = 0;
-        for (int i = this->ptr_nextfit; i < size; ++i)
-            if (tamanho <= this->livres[i].second)
-                found = true, this->ptr_nextfit = i, i = size;
-    }
 
     if (!found)
         return NULL;
@@ -119,7 +102,7 @@ char *meualoc::nextfit(unsigned short tamanho)
     return auxfit(this->ptr_nextfit, tamanho);
 }
 
-char *meualoc::auxfit(int index, unsigned short tamanho)
+char *meualoc::auxfit(size_t index, unsigned short tamanho)
 {
 
     /**
@@ -130,7 +113,6 @@ char *meualoc::auxfit(int index, unsigned short tamanho)
      *  - o número mágico.
      *  
     */
-
     SET_USED(tamanho, this->livres[index].first)
     SET_MAGIC(NUMERO_MAGICO, this->livres[index].first)
 
@@ -144,7 +126,6 @@ char *meualoc::auxfit(int index, unsigned short tamanho)
      * livres.
      * 
     */
-
     if (tamanho == this->livres[index].second)
         this->livres.erase(this->livres.begin() + index);
     else
@@ -164,40 +145,14 @@ char *meualoc::auxfit(int index, unsigned short tamanho)
          * livre.
          * 
         */
-
-        //  0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29
-        // [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-
-        #ifdef DEBUG
-            printf("Tamanho somado ao endereço: %hu\n", tamanho);
-        #endif
-
         this->livres[index].first += tamanho;
         this->livres[index].second -= tamanho;
     }
-
-    #ifdef DEBUG
-        puts("Blocos livres depois de alocação:");
-        for (auto t : this->livres)
-            printf("Endereço: %p tamanho: %hu\n", t.first, t.second);
-    #endif
 
     return mem + 0x4;
 }
 
 char *meualoc::verifica(char *ponteiro, int posicao)
-{
-
-    if (*(unsigned short *)(ponteiro - 0x4) < posicao)
-        return NULL;
-
-    if (*(unsigned short *)(ponteiro - 0x2) == NUMERO_MAGICO)
-        return ponteiro + posicao;
-    else
-        return NULL;
-}
-
-int meualoc::libera(char *ponteiro)
 {
 
     ushort used = 0;
@@ -206,8 +161,51 @@ int meualoc::libera(char *ponteiro)
     TAKE_USED(used, ponteiro)
     TAKE_MAGIC(magic, ponteiro)
 
-    if (magic != NUMERO_MAGICO)
+    /**
+     * 
+     * Se a posição, a partir do ponteiro base
+     * está fora dos limites, então essa é uma
+     * posição que não está alocada para esse
+     * ponteiro.
+     * 
+    */
+
+    if (used - 0x4 <= posicao)
+        return NULL;
+
+    /**
+     * 
+     * Se o número mágico está definido
+     * então **provavelmente** essa região está alocada.
+     * 
+    */
+    if (magic == NUMERO_MAGICO)
+        return ponteiro + posicao;
+    else
+        return NULL;
+}
+
+int meualoc::libera(char *ponteiro)
+{
+
+    if (ponteiro == NULL)
         return 0;
+
+    if (verifica(ponteiro) == NULL)
+        return 0;
+
+    /**
+     * 
+     * Como a região está sendo desalocada,
+     * é preciso redefinir o número mágico e
+     * o tamanho do bloco usado, caso contrário
+     * podem ocorrer falsos positivos em verificações
+     * de disponibilidade futuras.
+     * 
+    */
+
+    ushort used = 0;
+    TAKE_USED(used, ponteiro)
 
     SET_USED(0xFFFF, ponteiro - 0x4)
     SET_MAGIC(0xFFFF, ponteiro - 0x4)
@@ -221,36 +219,16 @@ int meualoc::libera(char *ponteiro)
 void meualoc::coalesce()
 {
 
-    #ifdef DEBUG
-        puts("Endereços dos espaços livres antes da ordenaçao:");
-        for (auto t : this->livres)
-            printf("Endereço: %p tamanho: %hu\n", t.first, t.second);
-    #endif
-
     std::sort(this->livres.begin(), this->livres.end());
 
-    #ifdef DEBUG
-        puts("Endereços dos espaços livres depois da ordenaçao:");
-        for (auto t : this->livres)
-            printf("Endereço: %p tamanho: %hu\n", t.first, t.second);
-    #endif
-
-    // 0x000000ff[10]0x00000100[15]0x00000102[15]...
-
-    //   0xff               0xf               0x10a
-    // [<char *, ushort>, <char *, ushort>, <char *, ushort>]
-
-    //   0xf               0xff      10       0x10a
-    // [<char *, ushort>, <char *, ushort>, <char *, ushort>]
-
-    int i = 1;
+    size_t i = 1UL;
     size_t size = this->livres.size();
 
     while (i < size)
     {
 
-        char *p = this->livres[i - 1].first;
-        ushort t = this->livres[i - 1].second;
+        char *p = this->livres[i - 1UL].first;
+        ushort t = this->livres[i - 1UL].second;
 
         /**
          * 
@@ -267,19 +245,22 @@ void meualoc::coalesce()
             --size;
             ushort x = this->livres[i].second;
 
-            #ifdef DEBUG
-                printf("Olhando: %p\n", p + t);
-            #endif
-
             // Aglutinação acontece aqui.
             this->livres.erase(this->livres.begin() + i);
-            this->livres[i - 1].second += x;
+            this->livres[i - 1UL].second += x;
         }
         else
             ++i;
     }
 
-    ptr_nextfit = 0;
+    /**
+     * 
+     * Reseta o ponteiro da nextfit porque
+     * não faz mais sentido depois dos elementos
+     * da lista de livres terem mudado de posição. 
+     * 
+    */
+    ptr_nextfit = 0UL;
 }
 
 void meualoc::imprimeDados()
@@ -289,12 +270,6 @@ void meualoc::imprimeDados()
     int maior = -1;
 
     size_t size = this->livres.size();
-
-    #ifdef DEBUG
-        puts("Endereços livres e seus tamanhos:");
-        for (auto t : this->livres)
-            printf("Endereço: %p tamanho: %hu\n", t.first, t.second);
-    #endif
 
     for (auto t : this->livres)
     {
